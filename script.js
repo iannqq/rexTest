4500da8bb7f9616b58279be52dce0c0d86982bed
iannqq <jaydeniscoolcajon@gmail.com>
2025-02-16 17:04:48 -0600

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 // Lists for each world and category
 
 // Pickaxes
-const W1Pickaxes = ["Default", "Poly Pickaxe", "Miner's Mallet", "Steel Sickle", "Stone Ravager", "Big Slammer", "Darkstone", "Crystalized Pickaxe", "Trinity Claymore", "57 Leaf Clover"];
+const W1Pickaxes = [
+  "Default", "Poly Pickaxe", "Miner's Mallet", "Steel Sickle",
+  "Stone Ravager", "Big Slammer", "Darkstone", "Crystalized Pickaxe",
+  "Trinity Claymore", "57 Leaf Clover"
+];
 const W2Pickaxes = ["W2 Pickaxe A", "W2 Pickaxe B", "W2 Pickaxe C"];
 
 // Left-Hand Gears
-const W1LeftGears = ["Core Frag", "Blazuine Molotov", "Accretium Fireball", "Temporum Timebomb", "Neptunium Nuke", "Combustal Clusterbomb", "Erodimium Bomb", "Suncindium Flashbang", "Luminatite Lantern", "Polarium Tunneller", "The Inktorb"];
+const W1LeftGears = [
+  "Core Frag", "Blazuine Molotov", "Accretium Fireball", "Temporum Timebomb",
+  "Neptunium Nuke", "Combustal Clusterbomb", "Erodimium Bomb",
+  "Suncindium Flashbang", "Luminatite Lantern", "Polarium Tunneller", "The Inktorb"
+];
 const W2LeftGears = ["W2 Left Gear A", "W2 Left Gear B"];
 
 // Right-Hand Gears
-const W1RightGears = ["Flashlight", "Jump Coil", "Speed Coil", "Thundarian Coil", "Acceleratium Coil", "Candilium Candle", "Elementonic"];
+const W1RightGears = [
+  "Flashlight", "Jump Coil", "Speed Coil", "Thundarian Coil",
+  "Acceleratium Coil", "Candilium Candle", "Elementonic"
+];
 const W2RightGears = ["W2 Right Gear A", "W2 Right Gear B"];
 
 // Helper function to pick a random item from an array
@@ -25,6 +36,7 @@ function generateLoadout() {
   
   // Combine Pickaxe options based on checkboxes
   document.querySelectorAll('.pickaxe-world:checked').forEach(checkbox => {
+    console.log("Pickaxe checkbox value:", checkbox.value);
     if (checkbox.value === "W1") {
       pickaxeOptions.push(...W1Pickaxes);
     }
@@ -35,6 +47,7 @@ function generateLoadout() {
   
   // Combine Left-Hand Gear options
   document.querySelectorAll('.leftgear-world:checked').forEach(checkbox => {
+    console.log("Left-Hand Gear checkbox value:", checkbox.value);
     if (checkbox.value === "W1") {
       leftGearOptions.push(...W1LeftGears);
     }
@@ -45,6 +58,7 @@ function generateLoadout() {
   
   // Combine Right-Hand Gear options
   document.querySelectorAll('.rightgear-world:checked').forEach(checkbox => {
+    console.log("Right-Hand Gear checkbox value:", checkbox.value);
     if (checkbox.value === "W1") {
       rightGearOptions.push(...W1RightGears);
     }
